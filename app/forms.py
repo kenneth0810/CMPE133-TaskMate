93b25ef8664028efe0019300a6b8c65de50f6195
Kenneth Nguyen <nguyenkenneth0810@gmail.com>
2024-10-22 20:38:09 -0700

    added task form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, SelectField, DateField, TimeField
 from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
@@ -14,3 +14,17 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
+
+class TaskForm(FlaskForm):
+    title = StringField('Task Title', validators=[DataRequired()])
+    description = StringField('Task Description', default=None)
+    priority = SelectField('Priority', choices=[
+        ('', 'None'),
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High')
+    ], default=None)
+    due_date = DateField('Due Date', format='%m-%d-%Y', default=None)
+    due_time = TimeField('Due Time', format='%I:%M %p', default=None)
+    is_completed = BooleanField('Complete Task', default=False)
+    submit = SubmitField('Create Task')
