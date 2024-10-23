a126f4b8eeccbbadec408619d9a675dee8d0494a
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2024-10-22 21:15:08 -0700

    Added Registeration+Login Functionality
    - Tested registering new user and logging in
    - Tested associating a saved task with a logged in user
    - Fixed misspelled variable name in model.py that caused an error when adding a new user (first_name vs firstname)
    - Added new task for planned work on a Tasks page
    - Added routes for an accounts/profile page and a tasks page

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, EmailField, PasswordField, BooleanField, SubmitField, IntegerField, DateField, TimeField
+from wtforms.validators import DataRequired, EqualTo, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
+
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description')
+    priority = IntegerField('Priority')
+    due_date = DateField('Due Date')
+    due_time = TimeField('Due Time')
