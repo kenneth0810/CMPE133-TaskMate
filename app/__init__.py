1c1f42ea83ba71bd193ccff9c336c94109900449
AlexNguyenSJSU <alexnguyen091203@gmail.com>
2024-10-29 10:20:16 -0700

    requirements.txt committed!

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +12,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 
 myapp.config.from_mapping(
-    SECRET_KEY = os.getenv('SECRET_KEY'),
+    SECRET_KEY = 'TaskMate',
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 )
@@ -20,6 +21,8 @@ db = SQLAlchemy(myapp)
 
 with myapp.app_context():
     db.create_all()
+
+migrate = Migrate(myapp, db)
 
 login = LoginManager(myapp)
 login.init_app(myapp)
